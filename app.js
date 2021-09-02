eda8387a0560ab35656fa6969d78de24e1a1742f
유하아민 <yuhaamin@yuhaamin-ui-MacBookPro.local>
2021-09-02 21:56:37 +0900

    vanilla_js

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const title = document.getElementById("title");
-console.dir(title); 
-title.innerText = "Got you!"
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.valu;
+    localStorage.setItem("user", username);
+    
+};
+
+paintGreeting(username) {
+    greeting.innerText = `hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreeting(savedUsername);
+}
+
