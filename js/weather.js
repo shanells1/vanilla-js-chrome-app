2165137ff9d77c33f95e8ee2b2a4d02274b6a74d
유하아민 <yuhaamin@yuhaamin-ui-MacBookPro.local>
2021-09-05 16:46:39 +0900

    vanilla-js

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,18 @@
+const API_KEY = "416aba008d3ac50f446551c44b05b3d3";
+
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    
-    console.log('you live it', lat, lng);
+    const lon = position.coords.longitude;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(URL)
+        .then((respones) => respones.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child")
+            const city = document.querySelector("#weather span:last-child")
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
 }
 function onGeoError() {
     alert("can't find you. No weather for you");
